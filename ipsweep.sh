1722c568503ad17181bd58449466119bfcf5ee40
root <root@localhost.localdomain>
2021-01-04 04:03:13 -0500

    added nmap for filtered ip address in iplist.txt

--- ipsweep.sh
+++ ipsweep.sh
@@ -9,3 +9,7 @@ for ip in `seq 1 254`; do
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" | tee -a iplist.txt &
 done
 fi
+
+
+'''nmap
+for ip in $(cat iplist.txt); do nmap -sS -p 80 -T 4 $ip & done'''
